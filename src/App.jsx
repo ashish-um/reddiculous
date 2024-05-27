d8a2454b2680a1dcfe7d10d370fced90dea62ec5
ashish-um <ashishumrao632005@gmail.com>
2024-05-27 14:54:23 +0530

    Using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Posts from "./components/Posts";
 import Nav from "./components/Nav";
 import { useState } from "react";
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate, Link, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Nav({ param }) {
   const navigate = useNavigate();
